fad309b1b0595ea9e74fc11872fbc9d2a7583c42
Rice Lin <rice.lin@icloud.com>
2025-04-02 16:53:55 +0800

    新增專案結構與基本功能，包括 AppCore 套件、計時器視圖、測試框架及資源檔案。新增 .gitignore 檔案以排除不必要的檔案。

---
+++ AppCore/Sources/AppCore/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Faster
+//
+//  Created by Rice Lin on 3/30/25.
+//
+
+import Foundation
+import SwiftData
+
+public final class Item {
+  var timestamp: Date
+
+  init(timestamp: Date) {
+    self.timestamp = timestamp
+  }
+}
---
+++ FasterTests/FasterTests.swift
@@ -0,0 +1,16 @@
+//
+//  FasterTests.swift
+//  FasterTests
+//
+//  Created by Rice Lin on 3/30/25.
+//
+
+import Testing
+
+struct FasterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
